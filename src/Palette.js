ae16a23f1dbbed35410221aaa4f8dca84357f1aa
filfran <felipefran13@gmail.com>
2021-07-12 13:05:21 +0300

    added Colorbox and basic styles

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 class Palette extends Component {
   render() {
+    const ColorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="Palette">
-        <div className="Palette-colors"></div>
+        <div className="Palette-colors">{ColorBoxes}</div>
       </div>
     );
   }
