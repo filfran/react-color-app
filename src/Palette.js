e34ed28293e9ae079b4ad359968e2d6cedeb582d
filfran <felipefran13@gmail.com>
2021-07-14 15:21:59 +0300

    style for the slider

--- src/Palette.js
+++ src/Palette.js
@@ -21,13 +21,27 @@ class Palette extends Component {
     );
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={this.state.level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={this.state.level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+            trackStyle={{ backgroundColor: "transparent" }}
+            handleStyle={{
+              backgroundColor: "green",
+              outline: "none",
+              border: "2px solid green",
+              boxShadow: "none",
+              width: "13px",
+              height: "13px",
+              marginLeft: "-7px",
+              marginTop: "-3px",
+            }}
+            railStyle={{ height: 8 }}
+          />
+        </div>
         <div className="Palette-colors">{ColorBoxes}</div>
       </div>
     );
